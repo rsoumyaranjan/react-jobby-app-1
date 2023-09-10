dbe7bde894ab5ec83e452e754a26019cc33562c7
rsoumyaranjan <soumyarn.rout@gmail.com>
2023-09-10 04:38:27 +0000

    First Attempt

---
+++ src/components/Jobs/index.js
@@ -0,0 +1,15 @@
+// JS Code
+import Header from '../Header'
+import JobProfileSection from '../JobProfileSection'
+import './index.css'
+
+const Jobs = () => (
+  <>
+    <Header />
+    <div className="job-profile-container">
+      <JobProfileSection />
+    </div>
+  </>
+)
+
+export default Jobs
